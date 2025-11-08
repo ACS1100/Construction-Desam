0c49ae7da28df9fdf461b66f1bfb8a1e9648aa4d
acs1100 <acs1100acs@gmail.com>
2025-11-08 10:43:28 +0530

    Changes in Filter

--- js/google-map.js
+++ js/google-map.js
@@ -1,13 +1,8 @@
-
 var google;
 
 function init() {
     // Basic options for a simple Google Map
-    // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
-    // var myLatlng = new google.maps.LatLng(40.71751, -73.990922);
     var myLatlng = new google.maps.LatLng(40.69847032728747, -73.9514422416687);
-    // 39.399872
-    // -8.224454
     
     var mapOptions = {
         // How zoomed in you want the map to start at (always required)
@@ -23,40 +18,42 @@ function init() {
                 "featureType": "administrative.country",
                 "elementType": "geometry",
                 "stylers": [
-                    {
-                        "visibility": "simplified"
-                    },
-                    {
-                        "hue": "#ff0000"
-                    }
+                    { "visibility": "simplified" },
+                    { "hue": "#ff0000" }
                 ]
             }
         ]
     };
 
-    
-
     // Get the HTML DOM element that will contain your map 
-    // We are using a div with id="map" seen below in the <body>
     var mapElement = document.getElementById('map');
 
-    // Create the Google Map using out element and options defined above
+    // SAFETY CHECK: If the element is not found, exit silently. 
+    // This prevents the error message if the script accidentally loads on a non-map page.
+    if (mapElement === null) {
+        // console.error("Map initialization skipped: HTML element with id='map' not found.");
+        return; 
+    }
+
+    // Create the Google Map using our element and options defined above
     var map = new google.maps.Map(mapElement, mapOptions);
     
     var addresses = ['New York'];
 
     for (var x = 0; x < addresses.length; x++) {
         $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+addresses[x]+'&sensor=false', null, function (data) {
-            var p = data.results[0].geometry.location
-            var latlng = new google.maps.LatLng(p.lat, p.lng);
-            new google.maps.Marker({
-                position: latlng,
-                map: map,
-                icon: 'images/loc.png'
-            });
-
+            if (data.results && data.results.length > 0) {
+                var p = data.results[0].geometry.location
+                var latlng = new google.maps.LatLng(p.lat, p.lng);
+                new google.maps.Marker({
+                    position: latlng,
+                    map: map,
+                    icon: 'images/loc.png'
+                });
+            }
         });
     }
-    
 }
-google.maps.event.addDomListener(window, 'load', init);
+
+// NOTE: The $(document).ready(init); line has been REMOVED.
+// The Google Maps API will call init() automatically.
